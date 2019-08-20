4b37762fe96e84a791d7090344f84bd6f0b9c85c
arunr04 <51693598+arunr04@users.noreply.github.com>
2019-08-20 18:15:11 +0530

    Update app2.py

--- Review-Service/app2.py
+++ Review-Service/app2.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    html = "<h3>Hello APP1 {name}!</h3>" \
+    html = "<h3>Hello APP2 {name}!</h3>" \
            "<b>Hostname:</b> {hostname}<br/>"
     return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname())
     
